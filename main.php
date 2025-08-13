0b66ee5befe59d243ee9ed294130fa8e97899d27
charlie22-dev <malinaocharlie74@gmail.com>
2025-08-13 01:26:55 +0000

    commit changes

---
+++ main.php
@@ -0,0 +1,17 @@
+<?php 
+$name = "Charlie mer libatod";
+$age = 19;
+$school = "Globalreciprocalcolleges";
+$course = "BSIT";
+$subject = "WEBSYS";
+$student = "yes";
+
+echo "Name: $name\n <br> ";
+echo "Age: $age\n <br> ";
+echo "School: $school\n <br> ";
+echo "Course: $course\n <br> ";
+echo "Subject: $subject\n<br>  ";
+echo "Student: " . ($student ? "Yes" : "No") . " \n<br> ";
+
+
+?>
